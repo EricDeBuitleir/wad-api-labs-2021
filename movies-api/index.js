71419745787ecff039b855efc4aa84c470b52132
EricDeBuitleir <72872237+EricDeBuitleir@users.noreply.github.com>
2022-11-24 18:15:05 +0000

    "Mongo lab Error Handling"
    Code implemented but not working fully yet

--- movies-api/index.js
+++ movies-api/index.js
@@ -8,6 +8,17 @@ import usersRouter from './api/users';
 
 dotenv.config();
 
+// eslint-disable-next-line no-unused-vars
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  // eslint-disable-next-line no-undef
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+};
+
 const app = express();
 
 // eslint-disable-next-line no-undef
@@ -18,6 +29,7 @@ app.use(express.json());
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
+app.use(errHandler);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
