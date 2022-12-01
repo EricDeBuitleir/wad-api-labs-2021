76d171f64cd492a3f5a95e9b2e869d8d4fa03ab3
EricDeBuitleir <72872237+EricDeBuitleir@users.noreply.github.com>
2022-12-01 12:09:21 +0000

    "Topic 10 - Object Referencing"

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import User from './userModel';
 import asyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
+import movieModel from '../movies/movieModel';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -49,4 +50,20 @@ router.post('/',asyncHandler( async (req, res, next) => {
         res.status(404).json({ code: 404, msg: 'Unable to Update User' });
     }
 });
+router.get('/:userName/favourites', asyncHandler( async (req, res) => {
+    const userName = req.params.userName;
+    const user = await User.findByUserName(userName).populate('favourites');
+    res.status(200).json(user.favourites);
+  }));
+  
+//Add a favourite. No Error Handling Yet. Can add duplicates too!
+router.post('/:userName/favourites', asyncHandler(async (req, res) => {
+    const newFavourite = req.body.id;
+    const userName = req.params.userName;
+    const movie = await movieModel.findByMovieDBId(newFavourite);
+    const user = await User.findByUserName(userName);
+    await user.favourites.push(movie._id);
+    await user.save(); 
+    res.status(201).json(user); 
+  }));
 export default router;
