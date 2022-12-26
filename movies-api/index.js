a60464e5ad1a1425b799b6a9557bbefb52a8893a
EricDeBuitleir <72872237+EricDeBuitleir@users.noreply.github.com>
2022-12-26 11:28:50 +0000

    re-added the labs as github glitched

---
+++ movies-api/index.js
@@ -0,0 +1,52 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
+import './db';
+import './seedData';
+import usersRouter from './api/users';
+import session from 'express-session';
+//import authenticate from './authenticate';
+import passport from './authenticate';
+
+dotenv.config();
+
+// eslint-disable-next-line no-unused-vars
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  // eslint-disable-next-line no-undef
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+};
+
+const app = express();
+
+// eslint-disable-next-line no-undef
+const port = process.env.PORT;
+
+//session middleware
+app.use(session({
+  secret: 'ilikecake',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+
+app.use(express.json());
+
+//update /api/Movie route
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
+
+
+app.use('/api/genres', genresRouter);
+app.use('/api/users', usersRouter);
+app.use(errHandler);
+
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
+
---
+++ movies-api/seedData/users.js
@@ -0,0 +1,16 @@
+const  users = [
+    {
+        'username': 'user1',
+        'password': 'test1',
+    },
+    {
+        'username': 'user2',
+        'password': 'test2',
+    },
+    {
+        'username': 'bruhMoment',
+        'password': 'password123',
+    },
+   
+];
+export default users;
